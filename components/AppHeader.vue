6ecddb9d49bfaa4ea56beaeb3554bf9ef1d44d96
Khafid Prayoga <khafidp@pm.me>
2025-06-02 22:43:49 +0700

    add navigation menu

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,4 +1,56 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui"
+
+
+const links: NavigationMenuItem[] = [
+  {
+    label: "Home",
+    to: "/",
+  },
+  {
+    label: "Blog",
+    to: "/blog",
+  },
+  {
+    label: "Resume",
+    to: "/resume",
+  },
+  {
+    label: "Showcase",
+    to: "/showcase",
+  },
+  {
+    label: "Story",
+    to: "/story",
+  },
+]
+
+</script>
+
 <template>
   <div>
+    <UNavigationMenu 
+    highlight
+    color="primary"
+    orientation="horizontal"
+    :items="links" 
+    variant="link"
+    :ui="{
+      link: 'py-1',
+      linkLabel: 'text-lg font-bold',
+      item: 'pr-3',
+    }"
+    >
+    <!-- todo add: color schema mode button moon-sun -->
+    <!-- <template #list-trailing> -->
+    <!-- </template> -->
+    </UNavigationMenu>
+    
   </div>
 </template>
+
+<style>
+a[aria-current="page"] {
+  @apply underline decoration-1 underline-offset-6;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,11 @@
 </script>
 
 <template>
-    <UContainer class="w-3xl mx-auto flex flex-col border-2 border-red-500 px-5 py-5">
+    <UContainer class="w-3xl mx-auto flex flex-col py-5 px-5">
       <AppHeader />
-      <slot />
+      <div class="md:min-h-screen py-10">
+        <slot />
+      </div>
       <AppFooter/>
     </UContainer>
 </template>
